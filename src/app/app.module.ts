98042247c0696c4431563dd07afffaab0218c527
educhinski <educhinski650@gmail.com>
2021-01-08 09:51:42 +0300

    Used observable and reactive extensions, http requests and error handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import {StarComponent} from './shared/star.component';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import {StarComponent} from './shared/star.component';
     ConvertToSpacesPipe,
     StarComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
