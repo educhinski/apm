f40a20a12880adadbdc87cffee5af94113611f3f
educhinski <educhinski650@gmail.com>
2021-01-09 23:33:19 +0300

    Added detail component details and necessary linked adjustments

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { IProduct } from './products.interface';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, tap, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -21,6 +21,12 @@ export class ProductService {
           catchError(this.handleError)
         )
       );
+  }
+
+  getProduct(id: number): Observable<IProduct | undefined> {
+    return this.getProducts().pipe(
+      map((products: IProduct[]) => products.find((p) => p.productId === id))
+    );
   }
 
   private handleError(err: HttpErrorResponse) {
